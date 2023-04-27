53f14e6f24fcac3fbfa68a04c32e8f0607575124
Ferdi Tarakcı <bilgi@ferditarakci.com.tr>
2023-04-28 02:53:12 +0300

    Added Prettier

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
 	<h1 class="title">Calculator App With Vue.js</h1>
 	<CalculatorApp />
 	<h2 class="repository">
-		<a href="https://github.com/ferditarakci/calculator-app-vue-js" title="Calculator App With Vue.js">GitHub Repository</a>
+		<a
+			href="https://github.com/ferditarakci/calculator-app-vue-js"
+			title="Calculator App With Vue.js"
+		>
+			GitHub Repository
+		</a>
 	</h2>
 </template>
 
--- src/components/CalculatorApp.vue
+++ src/components/CalculatorApp.vue
@@ -2,15 +2,33 @@
 	<div :class="styles.wrapper">
 		<div :class="styles.result">
 			<div v-if="total">
-				<strong :class="{ small: total.toString().length > 10 }">{{ total }}</strong>
+				<strong :class="{ small: total.toString().length > 10 }">
+					{{ total }}
+				</strong>
 			</div>
 			<div v-else>
 				{{ input }}
-				<strong :class="{ small: result.toString().length > 10 }">{{ result }}</strong>
+				<strong :class="{ small: result.toString().length > 10 }">
+					{{ result }}
+				</strong>
 			</div>
 		</div>
+		<div>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
+			accusamus neque! Exercitationem facere quia pariatur alias harum
+			illum voluptatum, cum cupiditate sit ea ex ipsam debitis assumenda
+			ad velit aliquid.
+		</div>
 		<div :class="styles.numpad">
-			<button @click="button(item.key)" v-for="(item, index) in keys" :key="index" :data-key="item.key" :class="styles[item.style]" type="button" aria-label="button">
+			<button
+				@click="button(item.key)"
+				v-for="(item, index) in keys"
+				:key="index"
+				:data-key="item.key"
+				:class="styles[item.style]"
+				type="button"
+				aria-label="button"
+			>
 				<span>
 					{{ item.key }}
 					<span>{{ item.title }}</span>
@@ -70,9 +88,16 @@ export default {
 
 			if (isNaN(parseInt(input.at(0)))) {
 				input = ''
-			} else if (input.at(0) === '0' && parseInt(input.at(1)) > 0 && parseInt(input.at(1)) < 10) {
+			} else if (
+				input.at(0) === '0' &&
+				parseInt(input.at(1)) > 0 &&
+				parseInt(input.at(1)) < 10
+			) {
 				input = input.slice(1)
-			} else if (isNaN(parseInt(input.at(-2))) && isNaN(parseInt(input.at(-1)))) {
+			} else if (
+				isNaN(parseInt(input.at(-2))) &&
+				isNaN(parseInt(input.at(-1)))
+			) {
 				input = input.slice(0, -2) + input.at(-1)
 			}
 
