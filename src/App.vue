bc1c21beb04a89f8526694bd6f70d4381cff3df9
Ferdi Tarakcı <bilgi@ferditarakci.com.tr>
2023-11-29 01:33:56 +0300

    Background style fix

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-	<h1 class="title">Calculator App With Vue.js</h1>
-	<CalculatorApp />
-	<h2 class="repository">
-		<a
-			href="https://github.com/ferditarakci/calculator-app-vue-js"
-			title="Calculator App With Vue.js"
-		>
-			GitHub Repository
-		</a>
-	</h2>
+	<div>
+		<h1 class="title">Calculator App With Vue.js</h1>
+		<CalculatorApp />
+		<h2 class="repository">
+			<a
+				href="https://github.com/ferditarakci/calculator-app-vue-js"
+				title="Calculator App With Vue.js"
+			>
+				GitHub Repository
+			</a>
+		</h2>
+	</div>
 </template>
 
 <script>
